e28d79a5a23f22bfbaa945808d5ad2744bae5a1b
poproar <poproar@users.noreply.github.com>
2020-01-27 11:45:11 -0800

    styling section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -23,7 +23,12 @@ class ActivityStore {
     const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     )
-    return sortedActivities;
+    // return sortedActivities;
+    return Object.entries(sortedActivities.reduce((activities, activity) => {
+      const date = activity.date.split('T')[0];
+      activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+      return activities;
+    }, {} as {[key: string]: IActivity[]}));
   }
 
   @action loadActivities = async () => {
