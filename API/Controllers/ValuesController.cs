17e9b0080d4aa3d807ce7b120cdb9132a3a1b45e
poproar <poproar@users.noreply.github.com>
2020-01-28 11:24:43 -0800

    return notFound value

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -32,6 +32,11 @@ namespace API.Controllers
         public async Task<ActionResult<string>> Get(int id)  // should string be Value
         {
             var value = await _context.Values.FindAsync(id);
+
+            if (value == null) 
+            {
+                 return NotFound();
+            }
             return Ok(value);
         }
 
