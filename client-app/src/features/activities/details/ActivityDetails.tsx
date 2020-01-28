e71a6e8f52af6fd26bb9c61fc4e47ded1375f07c
poproar <poproar@users.noreply.github.com>
2020-01-28 15:48:22 -0800

    show activity not found for debugging

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -24,8 +24,10 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
     loadActivity(match.params.id)
   }, [loadActivity, match.params.id, history]);
 
-  if (loadingInitial || !activity)
-    return <LoadingComponent content='Loading activity...' />;
+  if (loadingInitial) 
+  return <LoadingComponent content='Loading activity...' />;
+
+  if (!activity) return <h2>Activity not found</h2>;
 
   return (
     <Grid>
