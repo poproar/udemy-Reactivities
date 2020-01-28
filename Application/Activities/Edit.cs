b5c9c4998b2bad5d36c33539f23574e19ae389ed
poproar <poproar@users.noreply.github.com>
2020-01-28 12:23:49 -0800

    Server side error handling

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using FluentValidation;
 using MediatR;
 using Persistence;
@@ -43,10 +45,10 @@ namespace Application.Activities
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Id);    
+                var activity = await _context.Activities.FindAsync(request.Id);
 
                 if (activity == null)
-                    throw new Exception("Could not find activity");
+                    throw new RestException(HttpStatusCode.NotFound, new {activity = "Not Found"});
 
                 activity.Title = request.Title ?? activity.Title;            
                 activity.Description = request.Description ?? activity.Description;            
