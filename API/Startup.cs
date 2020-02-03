79fad6e3c79aa0e855b96e50843989a052d79a79
poproar <poproar@users.noreply.github.com>
2020-02-03 11:20:03 -0800

    get username

--- API/Startup.cs
+++ API/Startup.cs
@@ -82,6 +82,7 @@ namespace API
             );
 
             services.AddScoped<IJwtGenerator, JwtGenerator>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
