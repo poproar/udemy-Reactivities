cb2aa3998cae173ddc223b4bf24cf714c085c1ac
poproar <poproar@users.noreply.github.com>
2020-02-03 15:22:01 -0800

    create modal

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -36,6 +36,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
 
   return (
     <Fragment>
+      <ModalContainer />      
       <ToastContainer position='bottom-center' />
       <Route exact path='/' component={Homepage} />
       <Route
--- client-app/src/app/stores/rootStore.ts
+++ client-app/src/app/stores/rootStore.ts
@@ -3,18 +3,21 @@ import UserStore from "./userStore";
 import { createContext } from "react";
 import { configure } from "mobx";
 import CommonStore from "./commonStore";
+import ModalStore from "./modalStore";
 
-configure({ enforceActions: 'always' });
+configure({ enforceActions: 'always'});
 
 export class RootStore {
     activityStore: ActivityStore;
     userStore: UserStore;
     commonStore: CommonStore;
+    modalStore: ModalStore;
 
     constructor() {
         this.activityStore = new ActivityStore(this);
         this.userStore = new UserStore(this);
         this.commonStore = new CommonStore(this);
+        this.modalStore = new ModalStore(this);
     }
 }
 
