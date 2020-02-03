cd6fbe888450c81774b49a241472f3ee0bf6c8e0
poproar <poproar@users.noreply.github.com>
2020-02-03 15:21:09 -0800

    fix display name error

--- client-app/src/app/models/user.ts
+++ client-app/src/app/models/user.ts
@@ -1,6 +1,6 @@
 export interface IUser {
     username: string;
-    displayname: string;
+    displayName: string;
     token: string;
     image?: string;
 }
@@ -8,6 +8,6 @@ export interface IUser {
 export interface IUserFormValues {
     email: string;
     password: string;
-    displayname?: string;
+    displayName?: string;
     username?: string;
 }
