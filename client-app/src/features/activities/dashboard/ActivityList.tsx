f46f6902a23e63e25fe6af0d4086ddc9e7cafc21
poproar <poproar@users.noreply.github.com>
2020-02-03 15:23:55 -0800

    format date

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -3,17 +3,18 @@ import { Item, Label } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import ActivityListItem from './ActivityListItem';
 import { RootStoreContext } from '../../../app/stores/rootStore';
+import {format} from 'date-fns';
 
 const ActivityList: React.FC = () => {
   const rootStore = useContext(RootStoreContext);
   const { activitiesByDate } = rootStore.activityStore;
-
+  
   return (
     <Fragment>
       {activitiesByDate.map(([group, activities]) => (
         <Fragment key={group}>
           <Label size='large' color='blue'>
-            {group}
+            {format(group, 'eeee do MMMM')}
           </Label>
           <Item.Group divided>
             {activities.map(activity => (
