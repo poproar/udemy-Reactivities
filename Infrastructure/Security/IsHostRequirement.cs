f9d3afe530b46dd486cff86691b47c408501c16f
poproar <poproar@users.noreply.github.com>
2020-02-04 14:16:52 -0800

    roll back to netstandard2

--- Infrastructure/Security/IsHostRequirement.cs
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -4,13 +4,13 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Persistence;
 
 namespace Infrastructure.Security
 {
     public class IsHostRequirement : IAuthorizationRequirement
     {
-
     }
 
     public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
@@ -25,22 +25,23 @@ namespace Infrastructure.Security
 
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
         {
-            var currentUsrerName = _httpContextAccessor.HttpContext.User?.Claims?.
-                SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+            if (context.Resource is AuthorizationFilterContext authContext)
+            {
+                var currentUserName = _httpContextAccessor.HttpContext.User?.Claims?.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
 
-            var activityId = Guid.Parse(
-                _httpContextAccessor.HttpContext.Request.RouteValues
-                .SingleOrDefault(x => x.Key == "id").Value.ToString()
-                );
+                var activityId = Guid.Parse(authContext.RouteData.Values["id"].ToString());
 
                 var activity = _context.Activities.FindAsync(activityId).Result;
 
                 var host = activity.UserActivities.FirstOrDefault(x => x.IsHost);
 
-                if (host?.AppUser.UserName == currentUsrerName)
+                if (host?.AppUser?.UserName == currentUserName)
                     context.Succeed(requirement);
+            } else {
+                context.Fail();
+            }
 
-                return Task.CompletedTask;
+            return Task.CompletedTask;
         }
     }
 }
