9ca83d87980db1d2542b0a68e16ecab8ae9a5d5c
poproar <poproar@users.noreply.github.com>
2020-02-04 15:52:34 -0800

    cloudinary upload interface

--- API/Startup.cs
+++ API/Startup.cs
@@ -95,7 +95,9 @@ namespace API
 
             services.AddScoped<IJwtGenerator, JwtGenerator>();
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
             services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
