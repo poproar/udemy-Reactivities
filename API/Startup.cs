403da7367ff2273596c699942ec3b441218cf7ee
poproar <poproar@users.noreply.github.com>
2020-02-06 14:55:05 -0800

    server token authentication for signalR

--- API/Startup.cs
+++ API/Startup.cs
@@ -92,6 +92,19 @@ namespace API
                         ValidateAudience = false,
                         ValidateIssuer = false
                     };
+                    opt.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
+                            {
+                                context.Token = accessToken;
+                            }
+                            return Task.CompletedTask;
+                        }
+                    }
                 }
             );
 
