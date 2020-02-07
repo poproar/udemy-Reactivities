40fa925fc726b60982ab46c4a483ddf1024ce6e6
poproar <poproar@users.noreply.github.com>
2020-02-07 13:45:20 -0800

    show semantic content in profile tabs

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Tab} from 'semantic-ui-react';
+import ProfilePhotos from './ProfilePhotos';
+import ProfileDescription from './ProfileDescription';
 
 const panes = [
-    {menuItem: 'About', render: () => <Tab.Pane>About content</Tab.Pane>},
-    {menuItem: 'Photos', render: () => <Tab.Pane>Photos content</Tab.Pane>}, 
+    {menuItem: 'About', render: () => <ProfileDescription />},
+    {menuItem: 'Photos', render: () => <ProfilePhotos />},
     {menuItem: 'Activities', render: () => <Tab.Pane>Activities content</Tab.Pane>},
     {menuItem: 'Followers', render: () => <Tab.Pane>Followers content</Tab.Pane>},
     {menuItem: 'Following', render: () => <Tab.Pane>Following content</Tab.Pane>},
@@ -15,7 +17,6 @@ const ProfileContent = () => {
             menu={{fluid: true, vertical: true}}
             menuPosition='right'
             panes={panes}
-            activeIndex={1}
         />
     )
 }
