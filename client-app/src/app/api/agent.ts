3cd19b288bf3fe7ff9a23667bdfe5e938014cdad
poproar <poproar@users.noreply.github.com>
2020-02-07 13:45:39 -0800

    agent fixes

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,9 +4,9 @@ import { history } from '../..';
 import { toast } from 'react-toastify';
 import { IUserFormValues, IUser } from '../models/user';
 import { request } from 'http';
-import { IProfile } from '../models/profile';
+import { IProfile, IPhoto } from '../models/profile';
 
-axios.defaults.baseURL = 'http://localhost:5000/api'
+axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.request.use((config) => {
     const token = window.localStorage.getItem('jwt');
@@ -17,25 +17,18 @@ axios.interceptors.request.use((config) => {
 })
 
 axios.interceptors.response.use(undefined, error => {
-    if (error.message === 'Network Error' && !error.response)
-    {
-        toast.info('Cannot connect - dotnet watch run');
+    if (error.message === 'Network Error' && !error.response) {
+        toast.error('Network error - make sure API is running!')
     }
     const {status, data, config} = error.response;
-    
-    if (status === 404)
-    {
-        history.push('/notfound');
+    if (status === 404) {
+        history.push('/notfound')
     }
-    
-    if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id'))
-    {
-        history.push('/notfound');
+    if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
+        history.push('/notfound')
     }
-    
-    if (status === 500 )
-    {
-        toast.error('Server says check terminal');
+    if (status === 500) {
+        toast.error('Server error - check the terminal for more info!')
     }
     throw error.response;
 })
@@ -44,11 +37,19 @@ const responseBody = (response: AxiosResponse) => response.data;
 
 const sleep = (ms: number) => (response: AxiosResponse) => 
     new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+
 const requests = {
     get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody) 
+    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+    postForm: (url: string, file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post(url, formData, {
+            headers: {'Content-type': 'multipart/form-data'}
+        }).then(responseBody)
+    }
 };
 
 const Activities = {
@@ -62,18 +63,21 @@ const Activities = {
 }
 
 const User = {
-    current: (): Promise<IUser> => requests.get(`/user`),
+    current: (): Promise<IUser> => requests.get('/user'),
     login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
     register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user),
 }
 
-const Profile = {
-    get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`)
-
+const Profiles = {
+    get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`),
+    uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
+    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`),
+    updateProfile: (profile: Partial<IProfile>) => requests.put(`/profiles`, profile)
 }
 
 export default {
     Activities,
     User,
-    Profile
+    Profiles
 }
