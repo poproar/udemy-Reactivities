2e643dd2d759b8b4c8cf307fb872d595d96eccd0
poproar <poproar@users.noreply.github.com>
2020-02-11 14:49:50 -0800

    show following in client

--- client-app/src/features/activities/dashboard/ActivityListItemAttendees.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendees.tsx
@@ -7,6 +7,11 @@ interface IProps {
   attendees: IAttendee[];
 }
 
+const styles = {
+  borderColor: 'orange',
+  borderWidth: 3
+}
+
 const ActivityListItemAttendees: React.FC<IProps> = ({ attendees }) => {
   return (
     <List horizontal>
@@ -19,6 +24,8 @@ const ActivityListItemAttendees: React.FC<IProps> = ({ attendees }) => {
                 size='mini'
                 circular
                 src={attendee.image || '/assets/user.png'}
+                bordered
+                style={attendee.following ? styles : null}
               />
             }
           />
